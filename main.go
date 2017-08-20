2b9d9e6fbc15e1e505ea7b70346d75d03ad57611
Dobroslav Slavenskoj <c_dslave@qualcomm.com>
2017-08-19 22:00:14 -0400

    updating commented out sections

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 import (
 	"github.com/marcusolsson/tui-go"
 	"github.com/wargarblgarbl/libgosubs/ass"
-	
+
 
 )
-
-
-
 
 func main() {
 
@@ -15,11 +12,13 @@ func main() {
 	content.SetColumnStretch(0, 1)
 	content.SetColumnStretch(1, 1)
 	content.SetColumnStretch(2, 4)
-	var (
-	//	start = tui.NewLabel("")
-	//	end = tui.NewLabel("")
+/*	var (
+
+	start = tui.NewLabel("")
+		end = tui.NewLabel("")
 		text = tui.NewLabel("")
 	)
+*/
 	header := tui.NewTable(0,0)
 	header.SetColumnStretch(0, 1)
 	header.SetColumnStretch(1, 1)
@@ -28,21 +27,22 @@ func main() {
 		tui.NewLabel("Start"),
 		tui.NewLabel("End"),
 		tui.NewLabel("Text"),
-	
+
 	)
 
-	
-//	content.AppendRow(
-//		tui.NewLabel("Start"),
-//		tui.NewLabel("End"),
-//		tui.NewLabel("Text"),
-		
-//	)
-	
+/*
+	content.AppendRow(
+		tui.NewLabel("Start"),
+		tui.NewLabel("End"),
+		tui.NewLabel("Text"),
+
+	)
+*/
+
 	load := ass.ParseAss("./test.ass")
 	for _, i := range load.Events.Body {
-	//	label := setChannelLabel(slackChan, false)
-	/*	content.Append(tui.NewHBox(
+	/*	label := setChannelLabel(slackChan, false)
+		content.Append(tui.NewHBox(
 			tui.NewLabel(i.Start+"|"+i.End),
 			tui.NewPadder(1, 0, tui.NewLabel("|")),
 			tui.NewLabel(i.Text),
@@ -51,24 +51,24 @@ func main() {
 */
 	//	fmt.Println(i)
 		content.AppendRow(
-			
+
 			tui.NewLabel(i.Start),
 			tui.NewLabel(i.End),
 			tui.NewLabel(i.Text),
-	
+
 		)
 			//tui.NewHBox(
 //			tui.NewLabel(i.Start),
 //			tui.NewLabel(i.End),
-	
-		
-		
+
+
+
 	}
 
 
 ////////
 
-	
+
 	input := tui.NewEntry()
 	input.SetFocused(true)
 
@@ -78,9 +78,9 @@ func main() {
 //	inputBox.SetBorder(true)
 //	inputBox.SetFocused(true)
 //	inputBox.SetSizePolicy(0, tui.Maximum)
-	
+
 	/////
-/*	
+/*
 	textbx := tui.NewGrid(0,0)
 	textbx.SetSizePolicy(tui.Maximum, tui.Maximum)
 
@@ -90,7 +90,7 @@ func main() {
 	textbx.SetColumnStretch(1, 1)
 */
 
-	
+
 	content.OnItemActivated(func(t *tui.Table) {
 	//.OnSelectionChanged(func(t *tui.Table) {
 		m := load.Events.Body[t.Selected()]
@@ -98,13 +98,13 @@ func main() {
 		input.SetFocused(true)
 	})
 
-		
+
 
 
 
 	content.Select(0)
 //	editor := tui.NewVBox(header, content, textbx, input)
-	
+
 //	editor.SetSizePolicy(tui.Expanding, tui.Expanding)
 	root := tui.NewVBox(header, content, input)
 
@@ -112,10 +112,10 @@ func main() {
 //	tui.DefaultFocusChain.Set(content, input)
 
 	ui := tui.New(root)
-	
+
 	ui.SetKeybinding("Esc", func() { ui.Quit() })
 	if err := ui.Run(); err != nil {
 		panic(err)
 	}
-	
+
 }
